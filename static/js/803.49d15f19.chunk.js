91f5e60c0691fd9577c4cfab8a43903f2799359b
aripluss <aripluss@i.ua>
2023-03-03 08:52:54 +0000

    Deploying to gh-pages from @ aripluss/goit-react-hw-08-phonebook@5d21c7711744d424e3e54fff789b45902dbe473d 🚀

---
+++ static/js/803.49d15f19.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[803],{7803:function(e,n,t){t.r(n),t.d(n,{default:function(){return r}});var o=t(1149),c=t(8445),i=t(184);function r(){return(0,i.jsxs)(o.N,{style:{display:"flex",flexDirection:"column",gap:"40px"},children:[(0,i.jsx)("p",{style:{fontSize:"30px"},children:"404 Page Not Found"}),(0,i.jsx)(c.vd,{to:"/",children:"GO HOME"})]})}}}]);
+//# sourceMappingURL=803.49d15f19.chunk.js.map
